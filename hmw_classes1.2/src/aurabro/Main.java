c30265df230fff8c3fdb64d3e3f600fcb03c39f6
Akramjon Sattorov <aura.sttn@gmail.com>
2020-11-12 14:19:17 +0500

    1.2 solved

---
+++ hmw_classes1.2/src/aurabro/Main.java
@@ -0,0 +1,8 @@
+package aurabro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Variables var1 = new Variables("Photographer","Camera");
+    }
+}
---
+++ hmw_classes1.2/src/aurabro/Variables.java
@@ -0,0 +1,10 @@
+package aurabro;
+
+public class Variables {
+    String name;
+    String lastName;
+    Variables(String name, String lastName ){
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
